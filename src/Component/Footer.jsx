8853f5be908b2c35ab8328c16020911acb901da1
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-05-01 03:14:29 +0100

    add the button text

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer=()=><footer>
     <ul className="netwoksContainer">
-        <li><button id='facebookItem'></button></li>
-        <li><button id='pinterestItem'></button></li>
-        <li><button id='instagramItem'></button></li>
+        <li><button id='facebookItem'>{' '}</button></li>
+        <li><button id='pinterestItem'>{' '}</button></li>
+        <li><button id='instagramItem'>{' '}</button></li>
     </ul>
 </footer>
 
